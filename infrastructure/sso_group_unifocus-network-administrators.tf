7254b95b8f891b4632a7481e22b095462ad003d5
Vaibhav Singh <91778487+VaibhavSingh79@users.noreply.github.com>
2026-04-18 16:22:10 +0530

    Delete infrastructure directory
    clearing before I push again

--- infrastructure/sso_group_unifocus-network-administrators.tf
+++
@@ -1,7 +0,0 @@
-# Generated SSO Group: Unifocus-Network-Administrators
-resource "aws_identitystore_group" "unifocus_network_administrators" {
-  display_name      = "Unifocus-Network-Administrators"
-  description       = "Auto-generated SSO Group for Unifocus-Network-Administrators"
-  identity_store_id = var.identity_store_id # Requires a variables.tf mapping
-}
-
